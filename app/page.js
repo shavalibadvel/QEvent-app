d3f453d9bf61ebc4e2f89e3007f4d65e99f1b67c
shavalibadvel <55553298+shavalibadvel@users.noreply.github.com>
2026-06-24 21:19:15 +0530

    changes

--- app/page.js
+++ app/page.js
@@ -1,26 +1,9 @@
-import Link from 'next/link'
-import { getServerSession } from "next-auth/next"
-import { options } from "./api/auth/[...nextauth]/route"
+import SwiperComponent from "@/components/SwiperComponent";
 
-export default async function Home() {
-  const session = await getServerSession(options)
-
+export default function HomePage() {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-4">
-      <h1 className="text-4xl font-bold mb-8">Next-auth Demo</h1>
-      {session ? (
-        <div className="space-y-4 text-center">
-          <p>Welcome, {session.user.name}!</p>
-          <Link href="/api/auth/signout" className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-            Sign Out
-          </Link>
-        </div>
-      ) : (
-        <Link href="/api/auth/signin" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Sign In with GitHub
-        </Link>
-      )}
-    </div>
-  )
+    <main>
+      <SwiperComponent />
+    </main>
+  );
 }
-
